00555808856887163b2dcb58c854f903c52773b4
yufen <pengyufenft@outlook.com>
2023-06-11 01:17:22 -0600

    changes

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float CameraMoveSpeed = 120.0f;
+    public GameObject CameraFollowObj;
+    private Vector3 FollowPOS;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public GameObject CameraObj;
+    public GameObject PlayerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothV;
+    public float smoothY;
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotX = rot.y;
+        rotY = rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputY = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputY + mouseY;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+
+        // Move the camera position smoothly towards the follow position
+        FollowPOS = CameraFollowObj.transform.position - (CameraObj.transform.forward * camDistanceZToPlayer) + (CameraObj.transform.up * camDistanceYToPlayer);
+        transform.position = Vector3.Lerp(transform.position, FollowPOS, Time.deltaTime * CameraMoveSpeed);
+    }
+}
